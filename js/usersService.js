fb3ee4b76c8a947a190174572ebf8abde7b7123e
keiner759villalba <kjvillalbal@ufpso.edu.co>
2025-06-04 16:14:13 -0500

    add user

--- js/usersService.js
+++ js/usersService.js
@@ -25,6 +25,7 @@ function getUsers(){
         if(response.status === 200){
             
            let listUsers = `
+                <button onclick="addUser()" type="button" class="btn btn-outline-success"><i class="fa-solid fa-user-plus"></i></button>
                 <table class="table">
                     <thead>
                     <tr>
@@ -133,4 +134,89 @@ function showModalUser(user){
     document.getElementById('showModal').innerHTML = modalUser
     const modal = new bootstrap.Modal(document.getElementById('modalUser'))
     modal.show()
+}
+function addUser() {
+    const modalUserAdd = `
+        <div class="modal fade" id="showModalUserAdd" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Ver Usuario</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="card">
+                    <div class="card-body">
+                    <form id="formAddUser">
+                        <div class="row">
+                            <div class="col">
+                                <input type="text" id="first_name" class="form-control" placeholder="Primer nombre" aria-label="First name" required>
+                            </div>
+                            <div class="col">
+                                <input type="text" id="last_name" class="form-control" placeholder="Apellidos" aria-label="Last name" required>
+                            </div>
+                        </div>
+
+                        <div class="row mt-3">
+                            <div class="col">
+                                <input type="email" id="email" class="form-control" placeholder="Correo" aria-label="First name" required>
+                            </div>
+                            <div class="col">
+                                <input type="url" id="avatar" class="form-control" placeholder="Link del avatar" aria-label="Last name" required>
+                            </div>
+                        </div>
+
+                        <div class="row mt-3 ">
+                            <div class="col text-center">
+                                <button onclick="saveUser()" type="button" class="btn btn-success"><i class="fa-solid fa-floppy-disk"></i> Guardar</button>
+                            </div>
+                        </div> 
+                    </form>
+                </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    `;
+    document.getElementById('showModal').innerHTML = modalUserAdd;
+    const modal = new bootstrap.Modal(document.getElementById('showModalUserAdd'));
+    modal.show();
+}
+
+function saveUser() {
+    const form = document.getElementById('formAddUser');
+    if (form.checkValidity()) {
+        const firstName = document.getElementById('first_name').value;
+        const lastName = document.getElementById('last_name').value;
+        const email = document.getElementById('email').value;
+        const avatar = document.getElementById('avatar').value;
+        const user = {firstName, lastName, email, avatar};
+
+        const REQRES_ENDPOINT = 'https://dummyjson.com/users/add'
+        fetch(REQRES_ENDPOINT, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'x-api-key': 'reqres-free-v1'
+            },
+            body: JSON.stringify(user)
+        })
+        .then((response) =>response.json().then(data => ({status: response.status, info: data})))
+        .then(result => {
+            if (result.status === 201) {
+                document.getElementById('info').innerHTML = '<h3 class="text-success"><i class="fa-solid fa-check"></i> El usuario se guardo correctamente</h3>';
+            } else {
+                document.getElementById('info').innerHTML = '<h3><i class="fa-solid fa-x"></i> No se guardo el usuario en la api</h3>';
+                const modalId = document.getElementById('showModalUserAdd');
+                const modal = bootstrap.Modal.getInstance(modalId);
+                modal.hide();
+            }
+        })
+    } else {
+        form.reportValidity();
+    }
 }
